d7f8f47ee002bbb2197ffcbda6706e099e901ddc
zymoncone <szymon@sarnowicz.net>
2024-09-27 00:25:12 -0400

    add lamdba socket and update guest list check with dynamoDB check

--- src/assets/helper_functions.js
+++ src/assets/helper_functions.js
@@ -2,3 +2,33 @@ export const isMobileDevice = () => {
   console.log("checking if mobile device");
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 };
+
+export const readTable = (id) => {
+  const url = 'https://wrqj9e6vd1.execute-api.us-east-2.amazonaws.com/test/DynamoDBManager';
+  const data = {
+    operation: 'read',
+    payload: {
+      Key: {
+        id: id,
+      }
+    }
+  };
+
+  return fetch(url, {
+    method: 'POST', 
+    body: JSON.stringify(data), 
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => {
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return res.json();
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    throw error;
+  });
+}
--- src/components/RSVPForm/RSVPForm.js
+++ src/components/RSVPForm/RSVPForm.js
@@ -1,15 +1,39 @@
 import './RSVPForm.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { readTable } from '../../assets/helper_functions';
 
 function RSVPForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [attending, setAttending] = useState('');
+  const [guestMatch, setGuestMatch] = useState(false);
+  const [guestName, setGuestName] = useState('none');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     alert(`Name: ${name}, Email: ${email}, Attending: ${attending}`);
   };
+
+  useEffect(() => {
+    console.log(name);
+    
+    readTable(name)
+    .then(response => {
+    if (response && response.Item) {
+      console.log('Item exists:', response.Item);
+      setGuestMatch(true);
+      setGuestName(response.Item.guest);
+    } else {
+      console.log('Item does not exist');
+      setGuestMatch(false);
+      setGuestName('none');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+  }
+  , [name]);
 
   return (
     <form className="rsvp-form" onSubmit={handleSubmit}>
@@ -17,6 +41,12 @@ function RSVPForm() {
         <span className='label-text'>Name</span>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
       </label>
+      {guestMatch ? <p className="guest-match">You're on the list! Can't wait to see you!</p> : null}
+      {guestName !== 'none' ? 
+         <p className="guest-match">We see that you have {guestName} as a guest!</p> : 
+         null}
+      {guestName === 'none' && guestMatch ?
+          <p className="guest-match">No +1 but please come!</p> : null}
       <label>
         <span className='label-text'>Email</span>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
