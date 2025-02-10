9f6a24082aafd1f100eca6b17a63a32b80af4164
zymoncone <szymon@sarnowicz.net>
2025-02-10 10:57:12 -0500

    update gallery photos

--- src/assets/photos.js
+++ src/assets/photos.js
@@ -1,14 +1,15 @@
 
-const photos = ["https://i.imgur.com/vxUCkM0.jpeg",
-                "https://i.imgur.com/qppgoLo.jpeg",
+const photos = ["https://i.imgur.com/qppgoLo.jpeg",
                 "https://i.imgur.com/JBrrWxo.jpeg",
                 "https://i.imgur.com/CZGkG6K.jpeg",
                 "https://i.imgur.com/rMohY0f.jpeg",
                 "https://i.imgur.com/puQssqG.jpeg",
                 "https://i.imgur.com/nijLi9U.jpeg",
                 "https://i.imgur.com/Cchi9w1.jpeg",
-                "https://i.imgur.com/JuZc7P6.jpeg",
-                "https://i.imgur.com/3Q1hKty.jpeg",];
+                "https://i.imgur.com/advmF48.jpeg",
+                "https://i.imgur.com/3Q1hKty.jpeg",
+                "https://i.imgur.com/t9v8bu3.jpeg",];
+
 
 // add alts
 
